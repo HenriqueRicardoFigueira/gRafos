b7fcf4ba4c32241e42d97b65df57d933d043fd55
Henrique Ricardo Figueira <39686167+HenriqueRicardoFigueira@users.noreply.github.com>
2018-09-13 15:24:01 -0300

    Nova versao
    Nova versao, com classe Vertice adicionada.

--- Graph.py
+++ Graph.py
@@ -1,13 +1,48 @@
+import sys
+class Vertice:
+    def __init__(self, name, valor=0.0):
+        self.name = name
+        self.valor = valor
+        self.cor = "branco"
+        self.predecessor = None
+        self.tempDescoberta = -1
+        self.tempFinalizado = -1
+
+    def __str__(self):
+        return str(self.name)
 
 class Graph:
-    def __init__(self):
-        self.grafo = {}
+    def __init__(self, digrafo=0):
+        self.grafo = []
+        self.tempo = 0
+        self.digrafo = digrafo
+        self.vetorDistancia = []
+        self.adjacentes = []
 
     #insere Nos    
-    def insertNode(self,node):
-        self.grafo[node] = []
+    def insertNode(self, vertice):
+        if(1 if self.verificaVertice(vertice) == None else 0):
+            self.grafo.append(vertice)
+            self.adj.append([vertice,[]])
+
+    #nomePosicao
+    def achaPosicao(self, vertice):
+        aux = -1
+        for vert in self.grafo:
+            if(vert.name == vertice.name):
+                aux = self.grafo.index(vert)
+        return aux
     
-    #insere Aresta
+    #verificar se existe um vertice com esse nome
+    def verificaVertice(self, vertice):
+        aux = None
+        for vert in self.grafo:
+            if(vert.name == vertice.name):
+                aux = vert
+        return aux
+    
+    
+    '''#insere Aresta
     def insertAresta(self,node1,node2):
         if node2 not in self.grafo[node1]:
             self.grafo[node1].append(node2)
@@ -18,14 +53,31 @@ class Graph:
             self.grafo[node2].append(node1)
         else:
             return "Aresta ja exite"
+    '''
+    #insere aresta
+    def insereAresta(self, vertice, vertice2):
+        vertice = self.verificaVertice(vertice)
+        vertice2 = self.verificaVertice(vertice2)
+        index = self.grafo.index(vertice)
+        self.adjacentes[index][1].append(vertice2)
+
+        if self.digrafo == 0:
+            index2 = self.grafo.index(vertice2)
+            self.adjacentes[index2][1].append(vertice)
     
     #deleta Nos     
-    def deleteNode(self,node):
-        for target in self.grafo:
-            self.grafo[target].remove(node)
-        self.grafo.pop(node)
+    def deleteNode(self,vertice):
+        if(vertice in self.grafo):
+            index = self.grafo.index(vertice)
+            self.grafo.remove(vertice)
+            self.adjacentes.pop(index)
+
+        for vert in self.adjacentes:
+            for vert2 in vert[1]:
+                if(vert2 == vertice):
+                    vert[1].remove(vertice)
     
-    #deleta Aresta        
+    '''#deleta Aresta        
     def deleteAresta(self,node1,node2):
         if node2 in self.grafo[node1]:
             self.grafo[node1].remove(node2)
@@ -36,45 +88,57 @@ class Graph:
             self.grafo[node2].remove(node1)
         else:
             return "aresta nao encontrada"
-    
+    '''
     #printa o grafo
     def printGraph(self):
-        for indice in self.grafo:
-            print(str(indice)+": "+str(self.grafo[indice]))
+        print("\nGrafo: "+PyGraph.getOrdem()+":")
+        sys.stdout.write("[")
+        for vertice in PyGraph.grafo:
+            sys.stdout.write("- "+str(vertice)+" ")
+        print("]")
     
     #retorna Ordem do grafo
     def getOrdem(self):
-        return len(self.grafo)
+        return str(len(self.grafo))
     
     #retorna vertices do grafo
-    def getVertice(self):
-        return self.grafo.keys()
+    def getNos(self):
+        return self.grafo
 
     #def getNos()
     
     #retorna o Grau de um no
     def getGrau(self, node):
-        return len(self.grafo[node])
+        cont = 0
+        if(self.digrafo == 0):
+            for nos in Graph.adjacentes:
+                for vertice2 in nos[1]:
+                    if(vertice2 == no):
+                        cont+=1
+        return cont
 
     #retorna lista de nos adjacentes
     def getAdjacentes(self, node):
-        return self.grafo[node]
+        return self.adjacentes[self.grafo.index(no)][1]
 
     #retorna se o grafo e conexo ou nao
     def isConexo(self):
-        for target in self.grafo:
-            tamanho = len(self.grafo[target])
-            if  tamanho ==  0:
-                return False
-        return True
+        return True if (-1 not in self.vetorDistancia) else False
     
     #retorna se o grafo e completo
     def isCompleto(self):
-        for target in self.grafo:
-            tamanho = len(self.grafo[target])
-            if tamanho < tamanho - 1:
-                return False
-        return True
+        cont = 0
+        tam = len(self.grafo)
+        visita = []
+
+        for nos in self.adjacentes:
+            vista.append(nos[0])
+            for tam in nos[1]:
+                if tam not in visita:
+                    cont+=1
+                    vista.append(tam)
+            
+        return True if (tam*(tam-1))/2 == cont else False
 
     #def isArvore()
 
--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 from Graph import Graph
+from Graph import Vertice
 
 
 def main():
 
-    grafo = Graph()
-    grafo.insertNode(1)
-    grafo.insertNode(2)
-    grafo.insertNode(3)
-    grafo.insertNode(4)
-    grafo.insertNode(5)
-    grafo.insertAresta(1,2)
-    grafo.insertAresta(1,3)
-    grafo.insertAresta(2,3)
-    grafo.insertAresta(3,4)
-    grafo.insertAresta(4,5)
+    a = Vertice("a")
+    '''Graph.insertNode(a)
+    b = Vertice("b")
+    Graph.insertNode(b)
+    c = Vertice("c")
+    Graph.insertNode(c)
+    d = Vertice("d")
+    Graph.insertNode(d)
+    e = Vertice("e")
+    Graph.insertNode(e)
+    f = Vertice("f")
+    Graph.insertNode(f)
+    g = Vertice("g")
+    Graph.insertNode(g)
+    h = Vertice("h")
+    Graph.insertNode(h)
+
+    Graph.digrafo=1
+    #insere adjascencia
+    Graph.insereAresta(a,b)
+    Graph.insereAresta(a,f)
+    Graph.insereAresta(b,g)
+    Graph.insereAresta(b,c)
+    Graph.insereAresta(b,d)
+    Graph.insereAresta(d,e)
+    Graph.insereAresta(g,h)
+    grafo.printGraph()          '''
     
-    grafo.printGraph()
-    
-    print grafo.getVertice()
 main()
