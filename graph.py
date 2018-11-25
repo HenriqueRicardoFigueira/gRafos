16756844bab3f3614985cc2e509d24f2661722d5
marcos golom <viniciusgolom@gmail.com>
2018-11-25 20:43:46 -0200

    add plot graph

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
 import json
-from bson.json_util import dumps
+from bson.json_util import dumps, loads
 from bson.raw_bson import RawBSONDocument
 import bsonjs
 import networkx as nx
@@ -8,19 +8,19 @@ import matplotlib.pyplot as plt
 import networkx.drawing
 from pymongo import MongoClient
 
-def draw_graph(graph, labels=None, graph_layout='shell',
+def draw_graph(G, labels=None, graph_layout='shell',
                node_size=1600, node_color='blue', node_alpha=0.3,
                node_text_size=12,
                edge_color='blue', edge_alpha=0.3, edge_tickness=1,
                edge_text_pos=0.3,
                text_font='sans-serif'):
 
-    # create networkx graph
-    G = nx.Graph()
+    # # create networkx graph
+    # G = nx.Graph()
 
-    # add edges
-    for edge in graph:
-        G.add_edge(edge[0], edge[1])
+    # # add edges
+    # for edge in graph:
+    #     G.add_edge(edge[0], edge[1])
 
     # these are different layouts for the network you may try
     # shell seems to work best
@@ -34,41 +34,81 @@ def draw_graph(graph, labels=None, graph_layout='shell',
         graph_pos=nx.shell_layout(G)
 
     # draw graph
-    nx.draw_networkx_nodes(G,graph_pos,node_size=node_size, 
-                           alpha=node_alpha, node_color=node_color)
-    nx.draw_networkx_edges(G,graph_pos,width=edge_tickness,
-                           alpha=edge_alpha,edge_color=edge_color)
-    nx.draw_networkx_labels(G, graph_pos,font_size=node_text_size,
-                            font_family=text_font)
+    nx.draw_networkx_nodes(G,graph_pos,node_size=node_size, alpha=node_alpha, node_color=node_color)
+    nx.draw_networkx_edges(G,graph_pos,width=edge_tickness,alpha=edge_alpha,edge_color=edge_color)
+    nx.draw_networkx_labels(G, graph_pos,font_size=node_text_size,font_family=text_font)
 
     if labels is None:
-        labels = range(len(graph))
+        labels = range(400)
 
-    edge_labels = dict(zip(graph, labels))
-    nx.draw_networkx_edge_labels(G, graph_pos, edge_labels=edge_labels, 
-                                 label_pos=edge_text_pos)
+    # edge_labels = dict(zip(graph, labels))
+    nx.draw_networkx_edge_labels(G, graph_pos, label_pos=edge_text_pos)
 
     # show graph
     plt.show()
 #banco
-client = MongoClient('localhost', 27017,document_class=RawBSONDocument)
+client = MongoClient('localhost', 27017)
 db = client["steam_api"]
 
 user_id = "76561198004689792"
 
-response = db[user_id].find({},{"friend_List.user_id":1,"_id":0})
+G = nx.Graph()
+
+#cria pai
+G.add_node(user_id)
 
 
-responseFinal =  bsonjs.dumps(response)
+booking = dumps(db[user_id].find({},{"friend_List.user_id":1,"_id":0}))
+booking2 = loads(booking)
+friendsnv1 = booking2[0].get("friend_List")
 
-print type(responseFinal)
-print responseFinal
+cleanFriends = []
+for node in friendsnv1:
+    id = node.get("user_id")
+    cleanFriends.append(id)
+
+for node in cleanFriends:
+    G.add_node(node)
+    G.add_edge(user_id, node)
+
+# print len(cleanFriends)
+
+id = cleanFriends[0]
+
+for id in cleanFriends:
+    final = db[user_id].aggregate([
+        {"$match": {"friend_List.user_id": id}},
+        {"$addFields" : {"friend_List":{"$filter":{
+            "input": "$friend_List",
+            "as": "friend_List",
+            "cond": {"$eq": ["$$friend_List.user_id", id]}
+        }}}}
+    ])
+
+    bookingfinal = dumps(final)
+    bookingfinal2 = loads(bookingfinal)
+
+
+    userlistFriend = bookingfinal2[0].get("friend_List")[0].get("friend_List")
+    for node in userlistFriend:
+        G.add_node(node)
+        if node in cleanFriends:
+            G.add_edge(id, node)
+
+
+# arq = open("agregationReturn.json","w")
+# arq.write("{}".format(list(final)))
+
+nx.draw(G, with_labels=False, font_weight='bold')
+# nx.draw_shell(G, with_labels=True, font_weight='bold')
+plt.show()
+
+
 # graph = [(0, 1), (1, 5), (1, 7), (4, 5), (4, 8), (1, 6), (3, 7), (5, 9),
 #          (2, 4), (0, 4), (2, 5), (3, 6), (8, 9)]
 
 # # you may name your edge labels
 # labels = map(chr, range(65, 65+len(graph)))
-# #draw_graph(graph, labels)
 
 # # if edge labels is not specified, numeric labels (0, 1, 2...) will be used
 # draw_graph(graph)
