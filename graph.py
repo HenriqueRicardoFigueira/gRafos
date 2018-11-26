d13e8a045c3acdd062953f550e0cfb95ef8c165c
marcos golom <viniciusgolom@gmail.com>
2018-11-26 02:31:41 -0200

    add list(d)

--- graph.py
+++ graph.py
@@ -10,44 +10,6 @@ import networkx.drawing
 import operator
 from pymongo import MongoClient
 
-def draw_graph(G, labels=None, graph_layout='shell',
-               node_size=1600, node_color='blue', node_alpha=0.3,
-               node_text_size=12,
-               edge_color='blue', edge_alpha=0.3, edge_tickness=1,
-               edge_text_pos=0.3,
-               text_font='sans-serif'):
-
-    # # create networkx graph
-    # G = nx.Graph()
-
-    # # add edges
-    # for edge in graph:
-    #     G.add_edge(edge[0], edge[1])
-
-    # these are different layouts for the network you may try
-    # shell seems to work best
-    if graph_layout == 'spring':
-        graph_pos=nx.spring_layout(G)
-    elif graph_layout == 'spectral':
-        graph_pos=nx.spectral_layout(G)
-    elif graph_layout == 'random':
-        graph_pos=nx.random_layout(G)
-    else:
-        graph_pos=nx.shell_layout(G)
-
-    # draw graph
-    nx.draw_networkx_nodes(G,graph_pos,node_size=node_size, alpha=node_alpha, node_color=node_color)
-    nx.draw_networkx_edges(G,graph_pos,width=edge_tickness,alpha=edge_alpha,edge_color=edge_color)
-    nx.draw_networkx_labels(G, graph_pos,font_size=node_text_size,font_family=text_font)
-
-    if labels is None:
-        labels = range(400)
-
-    # edge_labels = dict(zip(graph, labels))
-    nx.draw_networkx_edge_labels(G, graph_pos, label_pos=edge_text_pos)
-
-    # show graph
-    plt.show()
 def byteify(input):
     if isinstance(input, dict):
         return {byteify(key): byteify(value)
@@ -149,17 +111,19 @@ sorted_x = sorted(aux.items(), key=operator.itemgetter(1))
 
 sort = sorted_x[-10:-1]
 
+sort_aux  = []
 for item in sort:
-    print type(item)
-    print "https://steamidfinder.com/lookup/{}/".format(item[0])
+    sort_aux.append(item[0])
+
 # nx.draw(G, with_labels=False, font_weight='bold')
 # nx.draw_shell(G, with_labels=True, font_weight='bold')
 # plt.show()
 #nx.draw(G, with_labels=False, font_weight='bold')
 # nx.draw_shell(G, with_labels=True, font_weight='bold')
 #plt.show()
-d = clique.max_clique(G)
-x = list(d)
+
+x = sort_aux
+
 x = byteify(x)
 
 gamesP = {}
